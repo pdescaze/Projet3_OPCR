7578db8a75d7ed7722cd1a637a06464dd2708e15
Pierre Descazeaux <descazeaux.pierre@gmail.com>
2017-08-24 11:29:45 +0200

    constants.py modified

---
+++ content/constants.py
@@ -0,0 +1,22 @@
+"""Game's constants """
+
+#File and pictures
+lab="Labyrinthe.json"
+background_picture="floors.png"
+Mcgyver="Mcgyver.png"
+Guardian="Guardian.png"
+Object1="Object1.png"
+Object2="Object2.png"
+Object3="Object3.png"
+Dead="blood.png"
+background_wall=(280,220,20,20)	
+background_arrival=(160,20,20,20)
+background_start=(160,20,20,20)
+background_field=(320,160,20,20)
+
+#Window
+sprite_size=40
+sprite_per_side=15
+window_side=sprite_size*sprite_per_side
+player_sprite_size=35
+
