7d9448b762f9a31e0fb878d6da181a42f020ce6d
Pierre Descazeaux <descazeaux.pierre@gmail.com>
2017-09-23 11:35:24 +0200

    Structure class updated, method test_position, load_victory and load_defeat added

--- content/classes.py
+++ content/classes.py
@@ -75,7 +75,42 @@ class Structure():
 		self.main_window.blit(self.frame_display, title_display_position)
 		pygame.display.flip()
 
-				
+
+	def test_position(self,position_player,position_guardian,catch_item1,catch_item2,catch_item3):
+
+		if position_player == position_guardian:
+			if catch_item1 == False or catch_item2 == False or catch_item3 == False:
+				self.status=1
+			else:
+				self.status=2
+		else:
+			pass
+
+		return self.status
+
+
+
+	def load_victory(self,victory_picture):
+
+		self.victory_picture=pygame.image.load(victory_picture).convert()
+		self.victory_picture.set_colorkey((255,255,255))
+		self.victory_picture=pygame.transform.scale(self.victory_picture,(window_side_x,window_side_y))
+		self.main_window.blit(self.victory_picture,(0,0))
+
+
+
+
+	def load_defeat(self,defeat_picture,artwork):
+
+		self.defeat_picture=pygame.image.load(defeat_picture).convert()
+		self.defeat_picture.set_colorkey((255,255,255))
+		self.defeat_picture=pygame.transform.scale(self.defeat_picture,(window_side_x,window_side_y))
+		self.main_window.blit(self.defeat_picture,(0,0))
+
+		self.pick_blood_picture=pygame.image.load(artwork).convert_alpha()
+		self.blood_picture=self.pick_blood_picture.subsurface(artwork_blood)
+		self.blood_picture=pygame.transform.scale(self.blood_picture,(object_sprite_size,object_sprite_size))
+		self.main_window.blit(self.blood_picture,(self.arrival_x,self.arrival_y))
 							
 
 
