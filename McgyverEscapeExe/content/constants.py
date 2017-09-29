a70bf906c83d680a0e21b707611466759e5e4c86
Pierre Descazeaux <descazeaux.pierre@gmail.com>
2017-09-29 22:16:01 +0200

    Standalone folder added

---
+++ McgyverEscapeExe/content/constants.py
@@ -0,0 +1,26 @@
+"""Game's constants """
+
+#File and pictures
+background_picture="pictures/floors.png"
+background_frame="pictures/blackbackground.png"
+background_wall=(280,220,20,20)	
+background_arrival=(160,20,20,20)
+background_start=(160,20,20,20)
+background_field=(320,160,20,20)
+artwork_object1=(32,0,32,32)
+artwork_object2=(160,0,32,32)
+artwork_blood=(192,0,32,32)
+window_title="McGyver escape"
+#Window
+sprite_size=40
+x_frame=30
+half_sprite=sprite_size/2
+sprite_per_side=15
+window_side_x=(sprite_size + x_frame) * sprite_per_side
+window_side_y=sprite_size * sprite_per_side
+frame_side=sprite_size * sprite_per_side
+player_sprite_size=35
+object_sprite_size=35
+inventory_display_position=(675,300)
+title_display_position=(700,50)
+
